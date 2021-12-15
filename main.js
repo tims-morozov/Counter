ef751527f7eb5e1bb48373cf5c3903cfd114f791
Timur Morozov <81633311+tims-morozov@users.noreply.github.com>
2021-12-15 20:43:57 +0300

    First_commit

--- main.js
+++ main.js
@@ -1,32 +1,51 @@
-let past = document.querySelector('.past'),
-    curr = document.querySelector('.current'),
-    rate = document.querySelector('.rate'),
-    res = document.querySelector('.result'),
-    btn = document.getElementsByClassName('button');
+const past = document.querySelector('.past'),
+      curr = document.querySelector('.current'),
+      rate = document.querySelector('.rate'),
+      btn = document.getElementsByClassName('button'),
+      res = document.getElementById('result');
 
-res.disabled = true;
+function displayError(message){
+    res.style.color = "red";
+    res.value = "";
+    res.innerHTML = `<b>Ошибка!</br>${message}</b>`;
+}
 
-btn[0].onclick = function countRes() {
-    let a = past.value,
-        b = curr.value,
-        c = rate.value;
-    
-    if (b > a){
-        res.value = (b-a)*c;
-    } else {
-        res.value = 'Ошибка';
-    };
+function countRes() {
+    const a = past.value,
+          b = curr.value,
+          c = rate.value;
+          
+        
+    if (!a || !b || !c){
+        displayError('Не все ячейки заполнены');
+        return;
+    }
+
+    const c1 = c.match (/[0-9]*\,?[0-9]*/),
+          c2 = a.match (/\d+/),
+          c3 = b.match (/\d+/);
+
+    if (c1 != c || c2 != a || c3 != b) { 
+        displayError('Некорректные данные, в поля нельзя вводить + - .');
+        return;
+    } 
+
+    if (Number(b) < Number(a)) {
+        displayError('Текущее меньше чем предыдущее');
+        return;
+    }   
+      
+    expression(a, b, c);
+};
+
+function expression(a, b, c){
+    res.style.color = "black";
+    res.value = "";
+    res.innerHTML = (b - a) * c;
 };
 
 document.addEventListener("keydown", function(event) {
-        let a = past.value,
-            b = curr.value,
-            c = rate.value;     
-
-        if (event.key === 'Enter' && b > a){
-            res.value = (b-a)*c;
-        } else if (event.key === 'Enter' && b < a) {
-            res.value = 'Ошибка';
-        }; 
-
+    if (event.key === 'Enter'){
+        countRes();
+    }
 });
